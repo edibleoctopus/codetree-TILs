af9e9abb56cd67e709056198a41c97a60f533537
edible_octopus <95427681+edibleoctopus@users.noreply.github.com>
2024-04-24 16:31:09 +0900

    Title: 돌 잘 치우기, Time: 631ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

--- 240424/돌 잘 치우기/clear-stones-well.cpp
+++ 240424/돌 잘 치우기/clear-stones-well.cpp
@@ -13,54 +13,48 @@ bool InRange(int x, int y) {
     return (x >= 0 && y >= 0 && x < n && y < n);
 }
 
-//bfs
-int getBFS(vector<vector<int>>& temp_map, vector<bool>& removed_rocks) {
-    vector<vector<bool>> visited(n, vector<bool>(n, false));
-    int num_count = 0;
-    for (int i = 0; i < k; i++) {
-        int r, c;
-        cin >> r >> c;
-        queue<pair<int, int>> q;
-        q.push({ r - 1, c - 1 });
-        while (!q.empty()) {
-            int x = q.front().first;
-            int y = q.front().second;
-            if (!visited[x][y]) {
-                visited[x][y] = true;
-                num_count++;
+int getBFS() {
+    int max_count = 0;
+    for (int mask = 0; mask < (1 << rocks.size()); mask++) {
+
+        if (__builtin_popcount(mask) != m) continue;
+
+        vector<vector<int>> temp_map = mmap;
+
+        for (int i = 0; i < rocks.size(); i++) {
+            if (mask & (1 << i)) {
+                temp_map[rocks[i].first][rocks[i].second] = 0;//돌 치우기
             }
-            q.pop();
-            for (int ij = 0; ij < 4; ij++) {
-                int nx = x + dx[ij];
-                int ny = y + dy[ij];
-                if (InRange(nx, ny) && !temp_map[nx][ny] && !visited[nx][ny]) {
-                    q.push({ nx, ny });
+        }
+
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        int num_count = 0;
+
+        for (int i = 0; i < k; i++) {
+            int r, c;
+            cin >> r >> c;
+            queue<pair<int, int>> q;
+            q.push({ r - 1, c - 1 });
+            while (!q.empty()) {
+                int x = q.front().first;
+                int y = q.front().second;
+                if (!visited[x][y]) {
+                    visited[x][y] = true;
+                    num_count++;
+                }
+                q.pop();
+                for (int ij = 0; ij < 4; ij++) {
+                    int nx = x + dx[ij];
+                    int ny = y + dy[ij];
+                    if (InRange(nx, ny) && !temp_map[nx][ny] && !visited[nx][ny]) {
+                        q.push({ nx, ny });
+                    }
                 }
             }
         }
-    }
-    return num_count;
-}
-
-//back tracking
-void generateCombinations(int idx, int count, vector<bool>& removed_rocks, int& max_count) {
-    if (count == m) {
-        vector<vector<int>> temp_map = mmap;
-        for (int i = 0; i < rocks.size(); i++) {
-            if (removed_rocks[i]) {
-                temp_map[rocks[i].first][rocks[i].second] = 0;
-            }
-        }
-        int num_count = getBFS(temp_map, removed_rocks);
         max_count = max(max_count, num_count);
-        return;
     }
-
-    for (int i = idx; i < rocks.size(); i++) {
-        removed_rocks[i] = true;
-        generateCombinations(i + 1, count + 1, removed_rocks, max_count);
-        removed_rocks[i] = false;
-    }
+    return max_count;
 }
 
 int main() {
@@ -76,10 +70,6 @@ int main() {
         }
     }
 
-    int max_count = 0;
-    vector<bool> removed_rocks(rocks.size(), false);
-    generateCombinations(0, 0, removed_rocks, max_count);
-
-    cout << max_count << '\n';
+    cout << getBFS() << '\n';
     return 0;
 }
