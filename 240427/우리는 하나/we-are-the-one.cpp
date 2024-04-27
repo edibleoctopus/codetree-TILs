f9694f729acb913825e17b73e92c115e87107bc8
edible_octopus <95427681+edibleoctopus@users.noreply.github.com>
2024-04-27 10:21:25 +0900

    Title: 우리는 하나, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240427/우리는 하나/we-are-the-one.cpp
+++ 240427/우리는 하나/we-are-the-one.cpp
@@ -5,31 +5,22 @@
 #include <algorithm>
 using namespace std;
 
-vector<vector<int>> mmap;
-bool visited[9][9];
 int n, k, u, d;
+vector<vector<int>> mmap;
 
-bool InRange(int x, int y) {
-    return (x >= 0 && y >= 0 && x < n && y < n);
+bool isValid(int x, int y) {
+    return x >= 0 && x < n && y >= 0 && y < n;
 }
 
-bool Cango(int x, int y, int nx, int ny) {
-    if (InRange(nx, ny)) {
-        int diff = abs(mmap[nx][ny] - mmap[x][y]);
-        if (diff >= u && diff <= d && !visited[nx][ny]) {
-            return true;
-        }
-    }
-    return false;
-}
-
-int BFS(int x, int y) {
+int bfs(int startX, int startY, vector<vector<bool>>& visited) {
+    int cnt = 1;
     queue<pair<int, int>> q;
-    q.push({ x, y });
-    visited[x][y] = true;
-    int c = 1;
+    q.push({startX, startY});
+    visited[startX][startY] = true;
 
-    int dx[] = { 0, 1, 0, -1 }, dy[] = { 1, 0, -1, 0 };
+    int dx[] = {0, 0, 1, -1};
+    int dy[] = {1, -1, 0, 0};
+
     while (!q.empty()) {
         int x = q.front().first;
         int y = q.front().second;
@@ -38,14 +29,19 @@ int BFS(int x, int y) {
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (Cango(x, y, nx, ny)) {
-                visited[nx][ny] = true;
-                c++;
-                q.push({ nx, ny });
+
+            if (isValid(nx, ny) && !visited[nx][ny]) {
+                int diff = abs(mmap[x][y] - mmap[nx][ny]);
+                if (diff >= u && diff <= d) {
+                    q.push({nx, ny});
+                    visited[nx][ny] = true;
+                    cnt++;
+                }
             }
         }
     }
-    return c;
+
+    return cnt;
 }
 
 int main() {
@@ -54,45 +50,40 @@ int main() {
     cout.tie(NULL);
 
     cin >> n >> k >> u >> d;
-
     mmap.resize(n, vector<int>(n));
-    vector<pair<int, int>> cities;
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> mmap[i][j];
-            cities.push_back({ i, j });
         }
     }
 
-    vector<pair<int, pair<int, int>>> city_counts;
-    for (int i = 0; i < cities.size(); i++) {
-        //visited 초기화
-        for (int j = 0; j < 9; j++) {
-            for (int k = 0; k < 9; k++) {
-                visited[j][k] = false;
-            }
+    vector<pair<int, pair<int, int>>> cities;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            vector<vector<bool>> visited(n, vector<bool>(n, false));
+            int cnt = bfs(i, j, visited);
+            cities.push_back({cnt, {i, j}});
         }
-        //모든 시작점에서 BFS 실시
-        int count = BFS(cities[i].first, cities[i].second);
-        city_counts.push_back({ count, {cities[i].first, cities[i].second}});
     }
-    //내림차순 정렬
-    sort(city_counts.rbegin(), city_counts.rend());
 
+    sort(cities.rbegin(), cities.rend());
+
+    vector<vector<bool>> selected(n, vector<bool>(n, false));
     int ans = 0;
-    vector<bool> selected(cities.size(), false);
-    for (int i = 0; i < k; i++) {
-        int count = city_counts[i].first;
-        int x = city_counts[i].second.first;
-        int y = city_counts[i].second.second;
 
-        if (!selected[x * n + y]) {
-            ans += count;
-            selected[x * n + y] = true;
+    for (int i = 0; i < cities.size() && k > 0; i++) {
+        int x = cities[i].second.first;
+        int y = cities[i].second.second;
+
+        if (!selected[x][y]) {
+            ans += cities[i].first;
+            selected[x][y] = true;
+            k--;
         }
     }
 
-    cout << ans;
+    cout << ans << endl;
 
     return 0;
 }
