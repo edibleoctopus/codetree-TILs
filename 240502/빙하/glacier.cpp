92cee3bc9c14fa9b62017163c4b639bbd72828cb
edible_octopus <95427681+edibleoctopus@users.noreply.github.com>
2024-05-02 01:15:43 +0900

    Title: 빙하, Time: 1094ms, Memory: 36MB, Status: Time Limit Exceed - CodeTree

--- 240502/빙하/glacier.cpp
+++ 240502/빙하/glacier.cpp
@@ -11,6 +11,7 @@ int dx[] = { 0,1,0,-1 }, dy[] = { 1,0,-1,0 };//동남서북
 
 vector<vector<int>> mmap;//맵 정보 저장
 vector<vector<bool>> visited;//방문 여부 저장(녹이는 물을 저장할 것.)
+queue<pair<int, int>> q;
 
 bool InRange(int x, int y)//영역 내에 있는지 확인
 {
@@ -35,9 +36,9 @@ void Init()
 
 void BFS()
 {
-    queue<pair<int, int>> q;
     q.push({0,0});//0,0에서 시작
     Init();
+
     while(!q.empty())
     {
         int x = q.front().first, y = q.front().second;
