ad5392354af4d14c582462e7be9b9e1a24940d6d
edible_octopus <95427681+edibleoctopus@users.noreply.github.com>
2024-05-02 01:46:10 +0900

    Title: 빙하, Time: 1101ms, Memory: 36MB, Status: Time Limit Exceed - CodeTree

--- 240502/빙하/glacier.cpp
+++ 240502/빙하/glacier.cpp
@@ -6,13 +6,8 @@ using namespace std;
 //1로 가로막혀있으면(못녹이는 물)visited를 못한다.
 //가로막혀있지 않은 물을 찾는 방식으로 풀기
 int n, m;
-int t = 0;
-int dx[] = { 0,1,0,-1 }, dy[] = { 1,0,-1,0 };//동남서북
-
 vector<vector<int>> mmap;//맵 정보 저장
-bool visited[200][200];
-queue<pair<int, int>> q;
-
+vector<vector<bool>> visited;//방문 여부 저장(녹이는 물을 저장할 것.)
 bool InRange(int x, int y)//영역 내에 있는지 확인
 {
     return (x>=0 && y>=0 && x<n && y<m);
@@ -23,27 +18,18 @@ bool Cango(int x, int y)
     return (InRange(x, y) && !mmap[x][y] && !visited[x][y]);//영역안에 있고, 방문하지 않았으며, 1이아니면(빙하)
 }
 
-void Init()
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            visited[i][j] = 0;
-        }
-    }
-}
+int t=0;
+int dx[] = {0,1,0,-1}, dy[] = {1,0,-1,0};//동남서북
 
 void BFS()
 {
-    Init();
-    q.push({0,0});//0,0에서 시작
-    visited[0][0] = true;
-
+    visited.assign(n, vector<bool>(m, false));//visited 초기화
+    queue<pair<int, int>> q;
+    q.push({0,0});
     while(!q.empty())
     {
-        pair<int, int> cc = q.front();
-        int x = cc.first, y = cc.second;
+        int x = q.front().first, y = q.front().second;
+        visited[x][y]=true;
         q.pop();
 
         for(int i=0;i<4;i++)
@@ -54,31 +40,34 @@ void BFS()
             if(Cango(nx,ny))
             {
                 q.push({nx,ny});
-                visited[nx][ny] = true;
             }
         }
     }
 }
 
-bool meltornot(int x, int y)
+bool meltornot(int i, int j)
 {
     for(int k=0;k<4;k++)
     {
-        int nx = x+dx[k];
-        int ny = y+dy[k];
+        int nx = i+dx[k];
+        int ny = j+dy[k];
 
-        if(InRange(nx, ny) && visited[nx][ny])
-        {        
-            return true;//녹이기     
+        if(InRange(nx, ny))
+        {
+            if(visited[nx][ny])//방문한 물이 있으면,
+            {
+                return true;//녹이기
+            }
         }
     }
     return false;//없으면 냅두기
 }
 
-int last_count_g;
+int last_count_g=0;
 
 void melt()
 {
+    last_count_g = 0; 
 
     for(int i=0;i<n;i++)
     {
@@ -94,38 +83,25 @@ void melt()
     }
 }
 
-void Simulate() {
-    t++;
-    last_count_g = 0;
-
-    //녹이는 물 확인하기(0,0에서 시작 )->BFS로 visited 채우기
-    BFS();
-
-    //녹이기(visited 물 근처에 빙하가 있으면 녹이기)
-    melt();
-
-}
-
 bool exist_g()
 {
-    for (int i = 0; i < n; i++)
+    for(int i=0;i<n;i++)
     {
-        for (int j = 0; j < m; j++)
+        for(int j=0;j<m;j++)
         {
-            if (mmap[i][j])//얼음이 있으면 계속
+            if(mmap[i][j])//얼음이 있으면 계속
             {
                 return true;
             }
         }
     }
     return false;//없으면 그만
-
 }
+
 int main()
 {
     cin>>n>>m;
     mmap.resize(n, vector<int>(m));
-
     //맵 정보 입력
     for(int i=0;i<n;i++)
     {
@@ -136,7 +112,13 @@ int main()
     }
     
     do{
-        Simulate();
+
+        //녹이는 물 확인하기(0,0에서 시작 )->BFS로 visited 채우기
+        BFS();
+        //녹이기(visited 물 근처에 빙하가 있으면 녹이기)
+        melt();
+        t++;
+
     }while(exist_g());  //빙하가 다 녹았는지 확인하기
 
     //다 녹이는데 몇 초 걸렸고, 마지막에 몇 개 녹였는지 출력하기
