da5fe371f282589c2b1eefd41ab7cd9ef98f6292
edible_octopus <95427681+edibleoctopus@users.noreply.github.com>
2024-05-02 16:33:58 +0900

    Title: xor 결과 최대 만들기, Time: 15ms, Memory: 0MB, Status: Passed - CodeTree

--- 240502/xor 결과 최대 만들기/max-of-xor.cpp
+++ 240502/xor 결과 최대 만들기/max-of-xor.cpp
@@ -6,11 +6,12 @@ using namespace std;
 //xor 결과 최대 만들기(m 개 숫자를 뽑아 모두 XOR한 결과의 최댓값)
 int n, m;
 int answer=0;
+
 vector<int> vt;
 vector<int> select_i;
 void Bactraking(int idx, int depth)
 {
-    if(idx>=vt.size() || depth>m)//idx가 size를 ㅓ넘거나, m개 이상 고르면 return
+    if(idx > vt.size() || depth>m)//idx가 size를 넘거나, m개 이상 고르면 return
         return;
 
     if(depth == m)
@@ -19,16 +20,17 @@ void Bactraking(int idx, int depth)
         int total = select_i[0];
         for(int i=1;i<select_i.size();i++)
         {
-            total = total^select_i[i];
+            total = total ^ select_i[i];
         }
+
         answer = max(answer, total);
 
         return;
     }
-    select_i.push_back(vt[idx]);
-    Bactraking(idx, depth+1);//넣고
-    select_i.pop_back();
 
+    select_i.push_back(vt[idx]);
+    Bactraking(idx+1, depth+1);//넣고
+    select_i.pop_back();
     Bactraking(idx+1, depth);//빼고
 
 }
@@ -42,6 +44,7 @@ int main() {
     {
         cin>>vt[i];
     }
+
     Bactraking(0,0);
     cout<<answer;
     return 0;
